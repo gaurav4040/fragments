2ac29e913a30dbb6a3d6fa521687a08bf2f6c04c
gaurav4040 <jangragaurav593@gmail.com>
2024-12-22 15:41:48 +0530

    second

--- frags/src/App.jsx
+++ frags/src/App.jsx
@@ -1,20 +1,18 @@
-
+import ErrorMessage from "./components/errorMessage";
+import FoodItems from "./components/foodItems";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
+  let foodItems = ["Dal", "Paneer", "Milk", "Dry Fruit", "Curd", "Ghee"];
 
-  let foodItems =["Dal","Paneer","Milk","Dry Fruit","Curd","Ghee"];
-  let emptyMessage=foodItems.length===0?<h3>I am still Hungry</h3>:null;
   return (
     <>
-      <h1>Healthy Food</h1>
-      {emptyMessage}
-      <ul className="list-group">
-        {foodItems.map((item) => ( 
-          <li key={item} className="list-group-item">{item}</li>
-          ))}  
-      </ul>
+      <center>
+        <h1 className="foodHeading GH-span">Healthy Food</h1>
+      </center>
+      <ErrorMessage foodArray={foodItems} />
+      <FoodItems foodArray={foodItems} />
     </>
   );
 }
